3c240b3c61b8eaa3f938df724ca251c07d675907
marcia-t <tejedamarcia@gmail.com>
2018-10-21 19:24:00 -0300

    chars in main screen

--- fighters/characters/Pikachu.gd
+++ fighters/characters/Pikachu.gd
@@ -3,10 +3,6 @@ extends "res://fighters/Fighter.gd"
 
 func _ready():
 	self.set_name("Pikachu")
-	$Info/Stats/Defense/Physic.set_text("10")
-	$Info/Stats/Defense/Magic.set_text("20")
-	$Info/Stats/Attack/Physic.set_text("100")
-	$Info/Stats/Attack/Magic.set_text("250")
 	pass
 
 
